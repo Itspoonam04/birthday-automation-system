ab1b3dc94c28877b20f836514b17656ce9be02ce
itspoonam04 <sonampoonam010@gmail.com>
2026-01-31 21:06:23 +0530

    Set DEBUG False for production

--- birthdaysys/settings.py
+++ birthdaysys/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%^v^$_h=er@j39=puoa=ca+wh^tt(l%c^d+75q81vjgcbw38z5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
